24949b1c6dec36d37d2e3bcb814772c1acac69f5
Robert <robert.winterqvist@gmail.com>
2026-02-03 16:28:50 +0100

    Remove secret, ready for deploy

---
+++ utils.py
@@ -0,0 +1,8 @@
+﻿import secrets
+import string
+
+def generate_disc_id():
+    prefix = "DISC-"
+    alphabet = "ABCDEFGHJKLMNPRSTUVWXYZ23456789"
+    code = ''.join(secrets.choice(alphabet) for _ in range(6))
+    return prefix + code
